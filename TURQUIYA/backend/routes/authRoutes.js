ee0d79c9c81e77974b11f770c2fba40ee1254a37
Cacerez Jefferson <jeffersoncace0@gmail.com>
2026-07-10 19:24:52 -0500

    Delete TURQUIYA directory

--- TURQUIYA/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registrarUsuario } from '../controlers/authController.js';
-
-const router = express.Router();
-
-router.post('/registro', registrarUsuario);
-
-export default router;
--- TURQUIYA/frontend/js/services/api.js
+++
@@ -1,5 +0,0 @@
-const API='http://localhost:3000';
-
-export async function consultar(endpoint){
-return fetch(API+endpoint);
-}
