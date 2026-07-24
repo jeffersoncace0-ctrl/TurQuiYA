9897ddfcea956a4b4d2990049687716b4c8359e5
jheffer <cacerezjefferson@gmail.com>
2026-07-24 08:52:43 -0500

    lol

--- frontend/js/views/login.js
+++
@@ -1,9 +0,0 @@
-export function login(){
-return `
-<div class="card">
-<h2>Inicio de sesion</h2>
-<input placeholder="Correo">
-<input type="password" placeholder="Contraseña">
-<button>Ingresar</button>
-</div>`;
-}
